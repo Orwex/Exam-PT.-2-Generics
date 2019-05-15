5b61e14578b98687a5704823e0138566aaa1e6b8
Orwex <44752155+Orwex@users.noreply.github.com>
2019-05-15 19:06:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _150519ExamOr
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
